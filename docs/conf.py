65b345a7eabe47e56e3c6cf6b1be9c805eecef15
Kevin Geil <info@placidsecurity.com>
2024-05-22 09:49:39 -0400

    adding index.md

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project = 'PowIRShell'
 version = '0.1'
 release = '0.1'
-master_doc = 'README'
+master_doc = 'index'
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
